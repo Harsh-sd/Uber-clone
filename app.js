6511796bce9721aeb2805a8b27efd1d593294814
Harsh-sd <harshsandilya05042002@gmail.com>
2025-01-09 04:22:28 +0530

    Added prefix for driver routes

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieparser = require("cookie-parser");
 const connectToDatabase = require("./config/db");
+const userRoutes = require("./routes/User");
+const driverRoutes = require("./routes/Driver");
 const app = express();
 dotenv.config();
 connectToDatabase();
 app.use(express.json());
+app.use(cookieparser());
+//prefix with /users ,/drivers
+app.use("/users", userRoutes);
+app.use("/drivers", driverRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on the port ${process.env.PORT}`);
