5d1f897ebbffdf4be16a01efa4db7a070920aecf
Harsh-sd <harshsandilya05042002@gmail.com>
2025-01-11 03:49:07 +0530

    added cors functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const cookieparser = require("cookie-parser");
 const connectToDatabase = require("./config/db");
 const userRoutes = require("./routes/User");
@@ -7,6 +8,12 @@ const driverRoutes = require("./routes/Driver");
 const app = express();
 dotenv.config();
 connectToDatabase();
+const corsOptions = {
+  origin: "http://localhost:5173", //frontend URL
+  credentials: true, // Allow credentials  for cookies, authorization
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieparser());
 //prefix with /users ,/drivers
