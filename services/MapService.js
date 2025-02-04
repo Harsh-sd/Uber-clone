c7e2f0b129e093641d934a02140d302a5939d0cb
Harsh-sd <harshsandilya05042002@gmail.com>
2025-02-04 23:48:00 +0530

    created function to get the services from google regarding distancetime , coordinates and suggestion through google api

---
+++ services/MapService.js
@@ -0,0 +1,76 @@
+const axios = require("axios");
+require("dotenv").config();
+
+async function getCoordinates(address) {
+  const apiKey = process.env.GOOGLE_MAPS_API;
+  //console.log(apiKey);
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+    address
+  )}&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    //console.log(response);
+    if (response.data.status === "OK") {
+      const location = response.data.results[0].geometry.location;
+      return {
+        lat: location.lat,
+        lng: location.lng,
+      };
+    } else {
+      throw new Error("Unable to fetch coordinates");
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+async function getDistanceTime(origin, destination) {
+  const apiKey = process.env.GOOGLE_MAPS_API;
+  const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(
+    origin
+  )}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    //console.log(response);
+    if (response.data.status === "OK") {
+      const element = response.data.rows[0].elements[0];
+      return {
+        distance: element.distance.text,
+        duration: element.duration.text,
+      };
+    } else {
+      throw new Error("Unable to fetch distance and time");
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+async function getAutoCompleteSuggestions(input) {
+  const apiKey = process.env.GOOGLE_MAPS_API;
+  const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(
+    input
+  )}&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    if (response.data.status === "OK") {
+      return response.data.predictions.map(
+        (prediction) => prediction.description
+      );
+    } else {
+      throw new Error("Unable to fetch autocomplete suggestions");
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+module.exports = {
+  getCoordinates,
+  getDistanceTime,
+  getAutoCompleteSuggestions,
+};
