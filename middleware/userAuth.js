f77fcc763a12777c44b396aed95e2132e848abab
Harsh-sd <harshsandilya05042002@gmail.com>
2025-04-13 10:02:49 +0530

    Uncommenting unnecessarily statements and changing module name

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -2,24 +2,23 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const Auth = async (req, res, next) => {
+const userAuth = async (req, res, next) => {
   try {
     //if we use token then there is no need to provide authorization in header
-    const token = req.cookies.token || req.headers.authorization;
-    console.log("cookie", req.cookies.token);
-    console.log("authorization", req.headers.authorization);
+    const token = req.headers.authorization;
+    //console.log("cookie", req.cookies.token);
+    //console.log("authorization", req.headers.authorization);
     if (!token) {
       return res
         .status(400)
         .json({ message: "Token not found", success: false });
     }
-
     // Verify the token using the secret key
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
 
     // Attach the decoded data to the req object
     req.id = decodedToken.userId;
-
+    //console.log(" Extracted userId:", req.id);
     next();
   } catch (error) {
     console.log("Internal server error:", error.message);
@@ -29,4 +28,4 @@ const Auth = async (req, res, next) => {
     });
   }
 };
-module.exports = Auth;
+module.exports = userAuth;
