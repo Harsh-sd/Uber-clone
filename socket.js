e1087099ab7bda89bd731682e2ccd42785d786e8
Harsh-sd <harshsandilya05042002@gmail.com>
2025-04-13 10:16:13 +0530

    socket file is supdated

--- socket.js
+++ socket.js
@@ -6,36 +6,64 @@ let io;
 const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "*", // Allow all origins
+      origin: "*", // Allowing all origins
       methods: ["GET", "POST"],
     },
   });
 
   io.on("connection", (socket) => {
-    console.log(`✅ User connected: ${socket.id}`);
+    console.log(` User connected: ${socket.id}`);
 
     socket.on("disconnect", () => {
-      console.log(`✅ User disconnected: ${socket.id}`);
+      console.log(` User disconnected: ${socket.id}`);
     });
     socket.on("join", async (data) => {
       const { userType, userId } = data;
-      console.log(`User ${userId} signed as ${userType}`);
+      console.log(`User ${userId} signed in as ${userType}`);
+
+      let updatedUser;
       if (userType === "User") {
-        await User.findByIdAndUpdate(userId, { socketId: socket.id });
+        updatedUser = await User.findByIdAndUpdate(
+          userId,
+          { socketId: socket.id },
+          { new: true }
+        );
       } else if (userType === "Driver") {
-        await Driver.findByIdAndUpdate(userId, { socketId: socket.id });
+        updatedUser = await Driver.findByIdAndUpdate(
+          userId,
+          { socketId: socket.id },
+          { new: true }
+        );
       }
+
+      console.log(" Updated User/Driver:", updatedUser);
+    });
+
+    socket.on("update-driver-location", async (data) => {
+      const { driverId, location } = data;
+
+      if (!location || !location.ltd || !location.lng) {
+        return socket.emit("error", {
+          message: "Location provided is Invalid",
+        });
+      }
+
+      await Driver.findByIdAndUpdate(driverId, {
+        location: {
+          ltd: location.ltd,
+          lng: location.lng,
+        },
+      });
     });
   });
 };
-const sendMessageToSocketId = (socketId, message) => {
-  console.log(message);
+const sendMessageToSocketId = (socketId, messageObject) => {
+  console.log("sendmessage:", messageObject);
 
   if (io) {
-    io.to(socketId).emit("message", message);
+    io.to(socketId).emit(messageObject.event, messageObject.data);
   } else {
     console.log("Socket.io not initialized.");
   }
 };
-
 module.exports = { initializeSocket, sendMessageToSocketId };
